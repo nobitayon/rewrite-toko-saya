f47e793ad1371c6f6d979434384333d72dd8c0fa
yonathanm10 <yonathanm@cocoindonesia.com>
2024-05-10 21:02:19 +0700

    landing page

---
+++ src/Components/Atoms/Slider/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Slider() {
+  return (
+    <div className='w-full my-10 h-60 bg-gray-200'>
+      
+    </div>
+  )
+}
---
+++ src/Components/Atoms/index.js
@@ -0,0 +1,7 @@
+export {default as Avatar} from './Avatar'
+export {default as Button} from './Button'
+export {default as Header} from './Header'
+export {default as Input} from './Input'
+export {default as Slider} from './Slider'
+export {default as TitleSection} from './TitleSection'
+export {default as Card} from './Card'
