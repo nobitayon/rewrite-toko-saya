a2e846da75cf36fbba3db14f095879a8dd07e75c
yonathanm10 <yonathanm@cocoindonesia.com>
2024-06-01 06:06:18 +0700

    try redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
-import {AllProduct, DetailProduct, Home, Login} from './Pages'
+import { AllProduct, DetailProduct, Home, Login } from './Pages'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Header } from './Components/Atoms'
 
 export default function App() {
   return (
     <BrowserRouter>
+      <div className='fixed top-0 left-0 w-full px-[10%] bg-gray-50/60 z-10'>
+        <Header />
+      </div>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/product' element={<AllProduct/>}/>
-        <Route path='/product/favorit' element={<AllProduct/>}/>
-        <Route path='/product/terlaris' element={<AllProduct/>}/>
-        <Route path='/product/:productId' element={<DetailProduct/>}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/product' element={<AllProduct />} />
+        <Route path='/product/favorit' element={<AllProduct />} />
+        <Route path='/product/terlaris' element={<AllProduct />} />
+        <Route path='/product/:productId' element={<DetailProduct />} />
       </Routes>
     </BrowserRouter>
   )
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,35 +1,60 @@
 import React from 'react'
 import { Header, Slider } from '../../Components/Atoms'
 import { ProductSection } from '../../Components/Molecules'
-import {products} from '../../dummy-data/data'
+import { products } from '../../dummy-data/data'
 import { useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment } from '../../service/counter/counterSlice'
 
 export default function Home() {
   let navigate = useNavigate()
-  const clickMoreTerlaris = ()=>{
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
+  const clickMoreTerlaris = () => {
     navigate("/product/terlaris")
   }
-  const clickMoreFavorit = ()=>{
+  const clickMoreFavorit = () => {
     navigate("/product/favorit")
   }
   return (
     <div className='px-[10%] pb-20 w-full min-h-screen'>
-      <div className='fixed top-0 left-0 w-full px-[10%] bg-gray-50/60 z-10'>
-        <Header/>  
-      </div>
+      {/* <div className='fixed top-0 left-0 w-full px-[10%] bg-gray-50/60 z-10'>
+        <Header />
+      </div> */}
       <div className='pt-20'></div>
-      <Slider/>
-      <ProductSection 
-        titleSection="Produk Terlaris" 
+      <Slider />
+      {/* hanya untuk contoh redux */}
+      <div className='pt-10'>
+        <div className='flex'>
+          <button
+            className='btn btn-secondary'
+            aria-label="Increment value"
+            onClick={() => dispatch(increment())}
+          >
+            Increment
+          </button>
+          <span className='px-10'>{count}</span>
+          <button
+            className='btn btn-secondary'
+            aria-label="Decrement value"
+            onClick={() => dispatch(decrement())}
+          >
+            Decrement
+          </button>
+        </div>
+      </div>
+      {/* hanya untuk contoh redux */}
+      <ProductSection
+        titleSection="Produk Terlaris"
         titleMore="Lihat Semua>"
         data={products}
-        clickMore={()=>clickMoreTerlaris()}
+        clickMore={() => clickMoreTerlaris()}
       />
-      <ProductSection 
-        titleSection="Produk Favorit" 
+      <ProductSection
+        titleSection="Produk Favorit"
         titleMore="Lihat Semua>"
         data={products}
-        clickMore={()=>clickMoreFavorit()}
+        clickMore={() => clickMoreFavorit()}
       />
     </div>
   )
